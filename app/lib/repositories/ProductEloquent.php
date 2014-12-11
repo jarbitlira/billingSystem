fe37b6da56c11b2c857914a906595c44d699b46b
Carlos Hernandez <honchkrow1995@gmail.com>
2014-12-10 18:46:29 -0600

    Product repository

---
+++ app/lib/repositories/ProductEloquent.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CarlosRenato
+ * Date: 10/12/2014
+ * Time: 12:02 AM
+ */
+
+namespace Repositories;
+use Product;
+
+class ProductEloquent extends \Repositories\BaseRepository implements ProductRepository{
+
+    public function __construct(){
+        $this->model = new Product;
+    }
+
+} 
---
+++ app/lib/repositories/ProductRepository.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CarlosRenato
+ * Date: 09/12/2014
+ * Time: 11:58 PM
+ */
+
+namespace Repositories;
+interface ProductRepository {
+
+}
