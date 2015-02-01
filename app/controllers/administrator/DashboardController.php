5ab9333c329e93f4626e4c1b87d7a687e33e0767
Carlos Hernandez <honchkrow1995@gmail.com>
2015-02-01 00:34:12 -0600

    Dashboard section
    - built
    - update to main layout

--- app/controllers/administrator/DashboardController.php
+++ app/controllers/administrator/DashboardController.php
@@ -7,18 +7,40 @@
  * Time: 10:11 PM
  */
 namespace Administrator;
+use Repositories\Administrator\ClientRepository;
+use Repositories\Administrator\InvoiceRepository;
+use Repositories\Administrator\ProductRepository;
+use Repositories\Administrator\ProviderRepository;
+
+
 class DashboardController extends \BaseController
 {
 
 //    protected $layout = 'layouts.dashboard';
     protected $layout = 'admin.layouts.main';
+    protected $product;
+    protected $client;
+    protected $invoice;
+    protected $provider;
 
-    public function __construct()
-    {
+    public function __construct(
+        ProductRepository $product,
+        ClientRepository $client,
+        InvoiceRepository $invoice,
+        ProviderRepository $provider
+    ) {
+        $this->product = $product;
+        $this->client = $client;
+        $this->invoice = $invoice;
+        $this->provider = $provider;
     }
 
     public function index()
     {
-        $this->layout->content = \View::make('admin.dashboard.index');
+        $clients = $this->client->lists();
+        $products = $this->product->lists();
+        $invoices = $this->invoice->lists();
+        $this->layout->content = \View::make('admin.dashboard.index', compact('clients', 'products', 'invoices'));
     }
+
 }
