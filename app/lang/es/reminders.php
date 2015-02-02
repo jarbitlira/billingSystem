8168cb194ad5e4a0e796e7a976af7e01215e40d8
Jarbit Lira <jarbitlira@outlook.com>
2015-02-02 01:40:30 -0600

    change default messages

--- app/lang/es/reminders.php
+++ app/lang/es/reminders.php
@@ -1,26 +1,22 @@
 <?php
-
 return array(
-
 	/*
-	|--------------------------------------------------------------------------
-	| Password Reminder Language Lines
-	|--------------------------------------------------------------------------
-	|
-	| The following language lines are the default lines which match reasons
-	| that are given by the password broker for a password update attempt
-	| has failed, such as for an invalid token or invalid new password.
-	|
-	*/
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+	"password" => "Las contraseñas deben contener al menos 6 caracteres y coincidir.",
 
-	"password" => "Contraseñas deben ser al menos de 6 caracteres y coincidir la confirmación.",
+	"user" => "No podemos encontrar a un usuario con ese correo electrónico.",
 
-	"user" => "No se encuentra ningún usuario con esa dirección de correo.",
+	"token" => "Este token de recuperación de contraseña es inválido.",
 
-	"token" => "La señal de reinicio de contraseña es invalida.",
+	"sent" => "¡Recordatorio de contraseña enviado!",
 
-	"sent" => "Recordatorio de contraseña enviada!",
-
-	"reset" => "Contraseña ha sido reiniciada!",
-
+	"reset" => "Su contraseña ha sido restablecida!",
 );
