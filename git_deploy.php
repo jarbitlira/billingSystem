56ca7c179e326b0e7c308cdbfd64d489438eb9fb
Ubuntu <jarbitlira@jarbitlira.jarbitlira.b1.internal.cloudapp.net>
2015-02-03 06:26:09 +0000

    change git_deploy

--- git_deploy.php
+++ git_deploy.php
@@ -9,16 +9,22 @@ $filePath = "app/storage/logs/github.log";
 
 try {
     $payload = json_decode($_REQUEST['payload']);
+
+    //log the request
+    file_put_contents($filePath, print_r($payload, TRUE));
+if ($payload->ref === 'refs/heads/master') {
+//     path to your site deployment script
+	echo shell_exec("git pull");
+	echo shell_exec("php artisan migrate --force");
+
+
+    //file_put_contents($filePath, exec('deploy.sh'));
+}
+
 } catch (Exception $e) {
     file_put_contents($filePath, $e->getMessage());
     exit(0);
 }
 
-//log the request
-file_put_contents($filePath, print_r($payload, TRUE));
 
 
-if ($payload->ref === 'refs/heads/master') {
-//     path to your site deployment script
-    file_put_contents($filePath, exec('deploy.sh'));
-}
