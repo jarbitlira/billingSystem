38aba40c2de8c140c2890bd460150e0aee733be2
Jarbit Lira <jarbitlira@outlook.com>
2015-02-03 00:39:31 -0600

    optimize hook

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-git pull
-composer update --no-dev --no-scripts
-php artisan dump-autoload
-php artisan migrate
--- git_deploy.php
+++ git_deploy.php
@@ -10,16 +10,34 @@ $filePath = "app/storage/logs/github.log";
 try {
     $payload = json_decode($_REQUEST['payload']);
 
+    $results = "";
+
     //log the request
     file_put_contents($filePath, print_r($payload, TRUE));
-if ($payload->ref === 'refs/heads/master') {
+    if ($payload->ref === 'refs/heads/master') {
 //     path to your site deployment script
-	echo shell_exec("git pull");
-	echo shell_exec("php artisan migrate --force");
-
-
-    //file_put_contents($filePath, exec('deploy.sh'));
-}
+        $results .= shell_exec("git pull");
+        $results .= "<br>";
+        $results .= shell_exec("composer dump-autoload");
+        $results .= "<br>";
+        $results .= shell_exec("php artisan dump-autoload");
+        $results .= "<br>";
+        $results .= shell_exec("php artisan migrate --force");
+        $results .= "<br>";
+        if (isset($payload->commits)) {
+            foreach ($payload->commits as $commit) {
+                if (isset($commit->modified)) {
+                    foreach ($commit->modified as $file) {
+                        if ($file == "composer.json") {
+                            $results .= shell_exec("composer update --no-dev --no-scripts");
+                            $results .= "<br>";
+                        }
+                    }
+                }
+            }
+        }
+        file_put_contents($filePath, str_replace("<br>", "\n", $results));
+    }
 
 } catch (Exception $e) {
     file_put_contents($filePath, $e->getMessage());
