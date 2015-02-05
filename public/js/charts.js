1ef70a460000d4797dd7818194fb158927df0a95
Carlos Hernandez <honchkrow1995@gmail.com>
2015-02-05 09:41:18 -0600

    Dashboard
    - Charts fixes

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,23 +1,26 @@
 /**
  * Created by CarlosRenato on 01/02/2015.
  */
-$(function(){
+$(function () {
     var sales = '';
     var topProducts = '';
     var browsers = '';
+
     google.setOnLoadCallback(drawChart);
+    google.setOnLoadCallback(drawPie);
+    google.setOnLoadCallback(drawBrowsers);
 
     function drawChart() {
         // Sales of last month
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'Month');
         data.addColumn('number', 'Sales');
-        if (sales == ''){
+        if (sales == '') {
             $.ajax({
                 type: 'GET',
                 url: '/json/lastMonthSales',
                 async: false,
-                success: function(response){
+                success: function (response) {
                     sales = response;
                 }
             });
@@ -31,39 +34,21 @@ $(function(){
         };
         var chart = new google.charts.Line(document.getElementById('monthSales'));
         chart.draw(data, options);
+    }
 
-        // Product Categories
-        if (topProducts == ''){
-            $.ajax({
-                url: '/json/topCategories',
-                type: 'GET',
-                asycn: false,
-                success: function(response){
-                    topProducts = response;
-                }
-            });
-        }
-        var pie = google.visualization.arrayToDataTable(topProducts);
-        var pieOptions = {
-            title: 'Top Product Categories',
-            is3D: true
-        };
-        var pieChart = new google.visualization.PieChart(document.getElementById('piechart'));
-        pieChart.draw(pie, pieOptions);
-
+    function drawBrowsers() {
         // Browsers
-        if (browsers == ''){
+        if (browsers == '') {
             $.ajax({
                 url: '/json/browserAccess',
                 type: 'GET',
                 async: false,
-                success: function(response){
+                success: function (response) {
                     browsers = response;
                 }
             })
         }
         var browserCont = google.visualization.arrayToDataTable(browsers);
-
         var optionsBrowsers = {
             title: 'Browser access',
             pieHole: 0.4,
@@ -73,10 +58,32 @@ $(function(){
 
         var pieBrowser = new google.visualization.PieChart(document.getElementById('donutBrowser'));
         pieBrowser.draw(browserCont, optionsBrowsers);
-
     }
 
-    $(window).resize(function(){
+    function drawPie() {
+        // Product Categories
+        if (topProducts == '') {
+            $.ajax({
+                url: '/json/topCategories',
+                type: 'GET',
+                asycn: false,
+                success: function (response) {
+                    topProducts = response;
+                }
+            });
+        }
+        var pie = google.visualization.arrayToDataTable(topProducts);
+        var pieOptions = {
+            title: 'Top Product Categories'
+            //is3D: true
+        };
+        var pieChart = new google.visualization.PieChart(document.getElementById('piechart'));
+        pieChart.draw(pie, pieOptions);
+    }
+
+    $(window).resize(function () {
         drawChart();
+        drawPie();
+        drawBrowsers();
     });
 });
